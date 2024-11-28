0413f7a5cc9e6a087b019839ed0b5cf0a4cd4448
shreyash_ghare <shreyashghare0019@gmail.com>
2024-11-28 22:13:45 +0530

    Code Updated3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 >My React App</h1>
+      <h1 className='header'>My React App</h1>
       <div>
         <h6>Welcome To My App</h6>
       </div>
